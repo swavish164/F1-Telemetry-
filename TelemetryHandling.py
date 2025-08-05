d3667483272a50bf36a80df02f3ba932225fc978
swavish164 <89749822+swavish164@users.noreply.github.com>
2025-08-05 22:05:38 +0100

    Getting more data

--- TelemetryHandling.py
+++ TelemetryHandling.py
@@ -9,10 +9,33 @@ lando = session.laps.pick_drivers('4').pick_fastest()
 circuit_info = session.get_circuit_info()
 tel = lando.get_telemetry()
 car_Tel = lando.get_car_data()
+pos_data = lando.get_pos_data()
+
 
 rpm = car_Tel['RPM']
 Speed = car_Tel['Speed']
 gear = car_Tel['nGear']
 throttle = car_Tel['Throttle']
 brake = car_Tel['Brake']
-print(rpm)
+time = car_Tel['Time']
+
+
+weather_data = session.weather_data
+
+lap_weather = weather_data.merge(car_Tel[['Time']], on='Time')
+
+air_temp = lap_weather['AirTemp']
+track_temp = lap_weather['TrackTemp']
+humidity = lap_weather['Humidity']
+wind_speed = lap_weather['WindSpeed']
+
+x = pos_data['X']
+y = pos_data['Y']
+
+plt.figure(figsize=(10, 8))
+plt.plot(x, y, label='Lando Norris Lap Path')
+plt.xlabel('X Position')
+plt.ylabel('Y Position')
+plt.title('Car Position Around Track')
+plt.legend()
+plt.show()
